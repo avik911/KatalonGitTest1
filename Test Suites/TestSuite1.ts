dd1616997e1c4e151591508469f5d5d27bb588ba
Vikrant Kumar <vikrant_kumar@epam.com>
2021-08-27 13:22:05 +0530

    test suite created

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>92060181-3ce1-4dcc-aa49-6d26cf918b24</testSuiteGuid>
+   <testCaseLink>
+      <guid>262b3704-3d36-48bb-a6b1-899316114694</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>541af9e9-a5c3-44ea-9e51-a966873f5720</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
